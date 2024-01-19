bec874cecfb8600d6a04651a56b3fafe2e33ff82
adnanbasalamah <adnan.b@gmail.com>
2024-01-19 13:18:50 +0000

    edit index.php for development

--- application/add_sales_order_modules.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `ospos_modules` (`name_lang_key`, `desc_lang_key`, `sort`, `module_id`) VALUES
-('module_sales_order', 'module_sales_order_desc', 75, 'sales_order');
-INSERT INTO `ospos_permissions` (`permission_id`, `module_id`, `location_id`) VALUES
-('sales_order', 'sales_order', NULL);
--- application/alter_sales_order_add_total_order.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `ospos_sales_order` ADD `total_order` DOUBLE NULL DEFAULT NULL AFTER `sale_type`;
