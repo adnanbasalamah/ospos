b325bbdb561fbdd0d6679bb3098210eae6175d5c
heri500 <heri500@gmail.com>
2024-01-23 22:12:08 +0800

    fix po form

--- application/views/purchase_order/po.php
+++ application/views/purchase_order/po.php
@@ -147,7 +147,8 @@ if (isset($success))
 							?>
 							<td>
                             <?php
-                            $item['receiving_quantity'] = !is_null($item['receiving_quantity']) ? $item['receiving_quantity'] : 1;
+                            $item['receiving_quantity'] = !empty($item['receiving_quantity']) ? to_quantity_decimals($item['receiving_quantity']) : 1;
+                            print 'receive qty : '.$item['receiving_quantity'];
                             ?>
 							<?php echo to_currency(($item['discount_type'] == PERCENT) ? $item['price']*$item['quantity']*$item['receiving_quantity'] - $item['price'] * $item['quantity'] * $item['receiving_quantity'] * $item['discount'] / 100 : $item['price']*$item['quantity']*$item['receiving_quantity'] - $item['discount']); ?></td>
 							<td><a href="javascript:$('#<?php echo 'cart_'.$line ?>').submit();" title=<?php echo $this->lang->line('receivings_update')?> ><span class="glyphicon glyphicon-refresh"></span></a></td>
